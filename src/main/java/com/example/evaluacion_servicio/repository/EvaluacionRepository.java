0ce56cb2dd30335174d78b18ca1e19705215e344
asmc1806 <a-cmoreno@hotmail.com>
2025-11-26 23:13:26 -0500

    Delete src directory

--- src/main/java/com/example/evaluacion_servicio/repository/EvaluacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.evaluacion_servicio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.evaluacion_servicio.model.Evaluacion;
-
-public interface EvaluacionRepository extends JpaRepository<Evaluacion, Long> {
-}
